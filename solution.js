cafea07103a3dcab70f6621512646ed70fce81f7
dimavetrov <ddvetrov94@gmail.com>
2015-02-03 21:29:32 +0200

    change

--- solution.js
+++ solution.js
@@ -8,7 +8,6 @@
   // For any other number N, its evenness is the same as N - 2.
   // Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
   // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
-
   if(number == 1)
     {  //odd
 	 return false;
